70d493f9098387d0597e64ec94dab76f27ca2fd8
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2025-12-17 12:24:20 +1100

    Cleanup navbar for fresh start

--- src/modules/root/ui/components/header.tsx
+++ src/modules/root/ui/components/header.tsx
@@ -1,7 +1,5 @@
 'use client'
 import React, {useRef} from 'react';
-import Link from "next/link";
-import {NAV_ITEMS} from "@/modules/root/constants/nav-items";
 
 
 const Header = () => {
@@ -9,32 +7,7 @@ const Header = () => {
 
     return (
         <nav ref={navRef} className='h-[var(--header-height)] mix-blend-difference fixed w-full'>
-            <div className='flex items-center justify-between h-full mx-4 lg:mx-10'>
-                <div className=''>
-                    <span className='text-4xl font-extrabold'>IFFA</span>
-                </div>
-                <div className='flex flex-row gap-24'>
-                    <div className='nav-items space-x-20 hidden lg:flex'>
-                        {NAV_ITEMS.map((link) => (
-                            <Link className='space-x-1' href={link.href} key={link.id}>
-                                <sup>
-                                    {link.id}
-                                </sup>
-                                <span>
-                                    {link.label}
-                                </span>
-                            </Link>
 
-                        ))}
-                    </div>
-                    <div id='contact-btn'>
-                        <Link  href='/contact' className=' uppercase'>
-                            <span>Contact</span>
-                        </Link>
-                    </div>
-                </div>
-
-            </div>
         </nav>
     );
 };
