92f1d02b803cc1c6ba27be03cfa9fe799ce5b4a6
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2025-12-17 11:02:06 +1100

    Setup GSAP config and isomorphic layout

--- src/modules/root/ui/components/header.tsx
+++ src/modules/root/ui/components/header.tsx
@@ -4,47 +4,14 @@ import Link from "next/link";
 import {NAV_ITEMS} from "@/modules/root/constants/nav-items";
 
 
-import { gsap } from '@/lib/gsap'
-import useIsomorphicLayoutEffect from "@/lib/gsap/custom-effect";
-
 const Header = () => {
     const navRef = useRef<HTMLDivElement>(null);
 
-    useIsomorphicLayoutEffect(() => {
-        if (!navRef.current) return;
-
-        const ctx = gsap.context(() => {
-            const mm = gsap.matchMedia();
-
-            mm.add('(min-width: 1024px)', () => {
-                const tl = gsap.timeline({
-                    scrollTrigger: {
-                        trigger: navRef.current,  // ok if you want header-based range
-                        start: 'top top',
-                        end: 'bottom 4rem',
-                        scrub: 1,
-                    },
-                });
-
-                // Use scoped selectors (descendants of navRef)
-                tl.to('.nav-items', { y: -100, duration: 0.6, ease: 'none' }, 0);
-                tl.to('#contact-btn', { x: -100, duration: 0.6, ease: 'none' }, 0);
-
-                return () => tl.kill();
-            });
-
-            return () => mm.revert();
-        }, navRef); // <-- scope here (critical)
-
-        return () => ctx.revert();
-    }, []);
-
-
     return (
         <nav ref={navRef} className='h-[var(--header-height)] mix-blend-difference fixed w-full'>
-            <div className='flex items-center justify-between h-full mx-1 lg:mx-10'>
+            <div className='flex items-center justify-between h-full mx-4 lg:mx-10'>
                 <div className=''>
-                    <span className='text-3xl font-extrabold'>IFFA Awards</span>
+                    <span className='text-4xl font-extrabold'>IFFA</span>
                 </div>
                 <div className='flex flex-row gap-24'>
                     <div className='nav-items space-x-20 hidden lg:flex'>
